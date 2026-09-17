4e122377e05369d6225b5f6791e7c4a6b446d0cc
Mukharjee Vemulapalli <144746926+369Apps@users.noreply.github.com>
2026-09-17 19:54:57 -0400

    Add app/sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
-/* GPS Kids Daily - offline cache, v4 (form POSTs bypass the worker) */
-var CACHE = "gpsk-daily-v4";
+/* GPS Kids Daily - offline cache, v5 (form POSTs bypass the worker) */
+var CACHE = "gpsk-daily-v5";
 var FILES = [
   "./",
   "./index.html",
