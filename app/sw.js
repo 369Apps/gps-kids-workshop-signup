fe65ff7948b046ad62db6dd6ce61fcf36346e366
Mukharjee Vemulapalli <144746926+369Apps@users.noreply.github.com>
2026-09-17 14:57:59 -0400

    GPS Kids Daily v1: sw.js

---
+++ app/sw.js
@@ -0,0 +1,38 @@
+/* GPS Kids Daily - offline cache, v1 */
+var CACHE = "gpsk-daily-v1";
+var FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./prompts.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) { return c.addAll(FILES); }).then(function () {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) {
+        if (k !== CACHE) return caches.delete(k);
+      }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      return hit || fetch(e.request);
+    })
+  );
+});
