a6bf0879f8f4e2fb3ba716d53972fbf8ecd72061
Mukharjee Vemulapalli <144746926+369Apps@users.noreply.github.com>
2026-09-20 12:12:55 -0400

    Add app/sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
 /* GPS Kids Daily - offline cache, v8 (adds leaderboard tab + hourly leaderboard.json) */
-var CACHE = "gpsk-daily-v9";
+var CACHE = "gpsk-daily-v10";
 var FILES = [
   "./",
   "./index.html",
